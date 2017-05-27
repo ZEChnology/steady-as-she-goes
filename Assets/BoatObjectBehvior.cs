cb5421069685dc4569f6d2479e6ca54e0f054e33
ericcline <cline6@gmail.com>
2017-05-27 17:00:47 -0500

    the boat lives

---
+++ Assets/BoatObjectBehvior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatObjectBehvior : MonoBehaviour {
+    public float movementSpeed = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("up"))
+        {
+            transform.Translate(Vector3.up * movementSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey("down"))
+        {
+            transform.Translate(Vector3.down* movementSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey("right"))
+        {
+            transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey("left"))
+        {
+            transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
+        }
+        else
+        {
+            print("fuck off");
+        }
+	}
+}
